df35e090e9c64f641b3445c104b602409713e259
thfield <thfield@gmail.com>
2014-04-25 13:31:55 -0700

    final edits

--- js/indepth.js
+++ js/indepth.js
@@ -1,21 +1,17 @@
 function factors(filePath) {
 var margin = {top: 10, right: 40, bottom: 40, left: 40},
-    margin2 = {top: 430, right: 40, bottom: 20, left: 40},
     width = 800 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom,
-    height2 = 500 - margin2.top - margin2.bottom;
+    height = 400 - margin.top - margin.bottom;
 
+  
 var parseDate = d3.time.format("%m/%d/%y").parse;
 
 var x = d3.time.scale().range([0, width]),
-    x2 = d3.time.scale().range([0, width]),
     y = d3.scale.linear().range([height, 0]),
-    y2 = d3.scale.linear().range([height2, 0]);
     ybar = d3.scale.linear().range([height, 0]).domain([0,1]);
     ytemp = d3.scale.linear().range([height, 0]).domain([37,77]);
 
 var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
     yAxis = d3.svg.axis().scale(y).orient("left"),
     yAxisTemp = d3.svg.axis().scale(ytemp).orient("right");
 
@@ -39,14 +35,9 @@ var area = d3.svg.area()
     .x(function(d) { return x(d.date); })
     .y0(height)
     .y1(function(d) { return y(d.rides); });
-var area2 = d3.svg.area()
-    .interpolate("basis")
-    .x(function(d) { return x2(d.date); })
-    .y0(height2)
-    .y1(function(d) { return y2(d.rides); });
+
 
 var baritup = d3.svg.area()
-    //.interpolate("monotone")
     .interpolate("step")
     .x(function(d) { return x(d.date); })
     .y0(height)
@@ -75,14 +66,7 @@ var focus = svg.append("g")
     .attr("class", "focus")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var context = svg.append("g")
-    .attr("class", "context")
-    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")")
-    .style("display", "none"); // hide brushing
-
-//
-d3.csv("data/"+filePath+".csv", function(error, data) {
-//d3.csv("data/factorsAll.csv", function(error, data) {
+  d3.csv("data/"+filePath+".csv", function(error, data) {
   
   color.domain(d3.keys(data[0]).filter(function(key) { return (key == "total" || key == "subscriber" || key =="customer"); }));
   avgs.domain(d3.keys(data[0]).filter(function(key) { return (key == "totalAvg" || key == "subscriberAvg" || key =="customerAvg"); }));
@@ -142,14 +126,7 @@ var tempCities = temps.domain().map(function(name) {
     d3.min(userType, function(c) { return d3.min(c.values, function(v) { return v.rides; }); }),
     d3.max(userType, function(c) { return d3.max(c.values, function(v) { return v.rides; }); })
   ]);
-  x2.domain(x.domain());
-  y2.domain(y.domain());
-/*  ytemp.domain([
-    d3.min(tempCities, function(c) { return d3.min(c.values, function(v) { return v.temp; }); }),
-    d3.max(tempCities, function(c) { return d3.max(c.values, function(v) { return v.temp; }); })
-  ]); */   
 
-//  
   focus.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -176,12 +153,7 @@ var tempCities = temps.domain().map(function(name) {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("ºF");
-    
-  context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height2 + ")")
-      .call(xAxis2);
-    
+ 
 //   
   var focusUsers = focus.selectAll(".focusUsers")
       .data(userType)
@@ -200,23 +172,12 @@ var tempCities = temps.domain().map(function(name) {
       .attr("class", "focusTemps")
       .style("display", "none");
 
-  var contextUsers = context.selectAll(".contextUsers")
-      .data(userType)
-    .enter().append("g")
-      .attr("class", "contextUsers");
-
   var notableDay = focus.selectAll(".notableDay")
       .data(specialDay)
     .enter().append("g")
       .attr("class", "notableDay");    
   
-//    
-  contextUsers.append("path")
-      .attr("class", "area")
-      .attr("d", function(d) { return area2(d.values); })
-      .style("stroke", function(d) { return color(d.name); })
-      .style("fill", function(d) { return color(d.name); });
-    
+//
   focusUsers.append("path")
       .attr("class", "area")
       .attr("d", function(d) { return area(d.values); })
@@ -255,15 +216,6 @@ var tempCities = temps.domain().map(function(name) {
       .attr("height", 18)
       .style("fill", function(d) { return color(d.name); });
     
-//  
-  /*
-  context.append("g")
-      .attr("class", "x brush")
-      .call(brush)
-    .selectAll("rect")
-      .attr("y", -6)
-      .attr("height", height2 + 7); 
-  /**/
   });
   
 } //end factors()
